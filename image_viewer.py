e64840b99cbb0f15426d36ccd21f7809e1088d3e
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2019-10-05 20:01:56 +0900

    [add] ROI cropping & printout into txt

--- image_viewer.py
+++ image_viewer.py
@@ -6,9 +6,25 @@ import matplotlib.pyplot as plt
 sampleimage = './sample-images/Image__2019-10-02__17-35-26.png'
 
 img = plt.imread(sampleimage)
-type(img) #=> numpy.ndarray
-img.size #=> image size
+
+roi = img[275:825,350:900]
+print(roi.shape) #=> ROI image size
+print(roi.ndim)
+print(roi[260][110])
 
 # Display image
 plt.imshow(img)
+plt.imshow(roi)
+
+# Display image as 2D-hist
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#H = ax.hist2d(roi, bins=65)
+#ax.set_xlabel('x')
+#ax.set_ylabel('y')
+#fig.colorbar(H[3],ax=ax)
+
+# Export image as TXT file
+np.savetxt('./sample-images/Image__2019-10-02__17-35-26.txt', roi, fmt='%.2f')
+
 plt.show()
