61350e728635d158fb07673349167617a433ef2e
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2019-10-12 21:43:26 +0900

    Display ROI image using OpenCV, then draws histo using ROOT

--- matrix.cpp
+++ matrix.cpp
@@ -1,7 +1,10 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <TCanvas.h>
+#include <TPad.h>
+#include <TStyle.h>
 #include <TASImage.h>
+#include <TH2D.h>
 #include <TRint.h>
 
 int main(int argc, char** argv){
@@ -16,20 +19,48 @@ int main(int argc, char** argv){
 	int width = 400;
 	int height = 400;
 
-	img = cv::imread(imagepath);
-	cv::cvtColor(img, dst, cv::COLOR_BGR2GRAY);
+
+	// ROOT section
+	//
 	TASImage *cv_image = new TASImage(imagepath);
+	UInt_t yPixels = cv_image->GetHeight();
+	UInt_t xPixels = cv_image->GetWidth();
+	UInt_t *argb = cv_image->GetArgbArray();
+	TH2D *h = new TH2D("h","Histogram",xPixels,-1,1,yPixels,-1,1);
+	for (int row=0; row<xPixels; ++row){
+		for (int col=0; col<yPixels; ++col){
+			int index = col*xPixels+row;
+			float gray = float(argb[index]&0xff)/256;
+			h->SetBinContent(row+1,yPixels-col,gray);
+		}
+	}
+
 	cv_image->DrawRectangle(topleft_x,topleft_y,width,height,"#00ff00",3);
 
 	TRint rootapp("app",&argc,argv);
 
-	TCanvas *c1 = new TCanvas();
+	TCanvas *c1 = new TCanvas("c1","c1",1280,512);
 	c1->Divide(2,1);
 
 	c1->cd(1);
 
 	cv_image->Draw();
 
+	c1->cd(2);
+
+//	TPad *hpad = new TPad("hpad","hpad",0,0.1,1,0.9,-1,-1);
+//	hpad->Draw();
+
+	TStyle *gStyle = new TStyle();
+	gStyle->SetPalette(53);
+	h->Draw("COLZ");
+
+
+
+	// OpenCV section
+	//
+	img = cv::imread(imagepath);
+	cv::cvtColor(img, dst, cv::COLOR_BGR2GRAY);
 
 //	std::cout << dst << std::endl; // retrieve entire matrix
 //	std::cout << dst.row(250) << std::endl; // retrieve specific row
