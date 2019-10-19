5462ea080277553f39dbb2472935b1808d8376c4
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2019-10-19 18:25:00 +0900

    [mod] implemented FuncAnimation method for streaming and updating histo/graph #3

--- simplehistostream.py
+++ simplehistostream.py
@@ -19,7 +19,6 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib import colors
 import matplotlib.patches as patches
-from matplotlib.widgets import Button
 from collections import deque
 import matplotlib.animation as animation
 
@@ -42,7 +41,10 @@ angle = 0
 log_time = 15
 
 # Number of images to be grabbed.
-countOfImagesToGrab = 100
+#countOfImagesToGrab = 100
+
+# Animation interval
+shoot_int = 100 # ms
 
 # The exit code of the sample application.
 exitCode = 0
@@ -55,6 +57,9 @@ try:
     # Print the model name of the camera.
     print("Using device ", camera.GetDeviceInfo().GetModelName())
 
+    print("Press 'q' to stop streaming")
+    print()
+
     # demonstrate some feature access
     new_width = camera.Width.GetValue() - camera.Width.GetInc()
     if new_width >= camera.Width.GetMin():
@@ -62,112 +67,111 @@ try:
 
     # The parameter MaxNumBuffer can be used to control the count of buffers
     # allocated for grabbing. The default value of this parameter is 10.
-    camera.MaxNumBuffer = 5
+#    camera.MaxNumBuffer = 5
+    camera.MaxNumBuffer = 10
 
     # Start the grabbing of c_countOfImagesToGrab images.
     # The camera device is parameterized with a default configuration which
     # sets up free-running continuous acquisition.
-    camera.StartGrabbingMax(countOfImagesToGrab)
+#    camera.StartGrabbingMax(countOfImagesToGrab)
+#    camera.StartGrabbing(pylon.GrabStrategy_OneByOne)
+    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+
+    # Retrieve sample image to set default canvas
+    take_one_shot = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+    sampleGrab = take_one_shot.Array
 
     # Create figure
     ax1 = plt.subplot(2,2,1)
-#    im1 = ax1.matshow()
+    im1 = ax1.matshow(sampleGrab)
     ax2 = plt.subplot(2,2,2)
-#    im2 = ax2.matshow()
+    im2 = ax2.matshow(sampleGrab[y_1:y_1+height+1, x_1:x_1+width+1]/maxscale)
     ax3 = plt.subplot(2,2,3)
+
+    # Set empty titles/labels
+    ax1.set_title('')
+    ax2.set_title('')
+    ax3.set_title('')
+    ax3.set_xlabel('')
+    ax3.set_ylabel('')
 
     # Prepare brightness lists
     time_start = time.time()
     time_elapsed = deque()
     roi_brightness = deque()
 
-    # Camera.StopGrabbing() is called automatically by the RetrieveResult() method
-    # when c_countOfImagesToGrab images have been retrieved.
-    counter = 0
-    while camera.IsGrabbing():
+    # Define each update
+    def update(i, ax1_title, ax2_title, ax3_title, ax3_xlabel, ax3_ylabel):
         # Wait for an image and then retrieve it. A timeout of 5000 ms is used.
         grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
 
         # Image grabbed successfully?
         if grabResult.GrabSucceeded():
-            def update(i):
-                # Access the image data.
-#               print("SizeX: ", grabResult.Width)
-#               print("SizeY: ", grabResult.Height)
-                img = grabResult.Array
-#               print("Gray value of first pixel: ", img[0, 0])
-#               print("Entire image: ", img)
+            plt.cla()
+            img = grabResult.Array
 
-                # Draw histogramized image
-                ax1.set_data(img,0)
-                current_time = datetime.strftime(datetime.now(),"%Y-%m-%d_%H-%M-%S-%f")
-                ax1.title('Obtained Image (%s)'%current_time)
-                if i == 1:
-                    cmap = cm.get_cmap('jet',4)
-                    bounds = np.linspace(0,maxscale,(maxscale+1)*20+1)
-                    ax1.colorbar(cmap=colors.ListedColormap(['b','g','y','r']),boundaries=bounds,norm=colors.BoundaryNorm(bounds,cmap.N))
-                ax1.tick_params(axis='x',which='both',top=False,labeltop=False,labelbottom=True)
-                roi = patches.Rectangle((x_1,y_1),width,height,angle,linewidth=1,edgecolor='r',facecolor='none')
-                ax1.gca().add_patch(roi)
+            # Draw histogramized image
+            acq_timestamp = datetime.strftime(datetime.now(),"%Y-%m-%d_%H-%M-%S-%f")
+            ax1.set_title(ax1_title + '(' + str(acq_timestamp) + ')')
+            im1.set_data(img)
+            ax1.tick_params(axis='x',which='both',top=False,labeltop=False,labelbottom=True)
+            roi = patches.Rectangle((x_1,y_1),width,height,angle,linewidth=1,edgecolor='r',facecolor='none')
+#            ax1.gca().add_patch(roi)
+            ax1.add_patch(roi)
 
-                # Draw the Zoomed image in the ROI
-                img_roi = img[y_1:y_1+height+1, x_1:x_1+width+1]/maxscale
-                del img
+            # Draw the Zoomed image in the ROI
+            img_roi = img[y_1:y_1+height+1, x_1:x_1+width+1]/maxscale
+            del img
+            gc.collect()
+            im2.set_data(img_roi)
+            ax2.set_title(ax2_title + '(x_1=' + str(x_1) + ', y_1=' + str(y_1) + ', w=' + str(width) + ', h=' + str(height) + ')')
+            ax2.tick_params(axis='x',which='both',top=False,labeltop=False,labelbottom=True)
+
+            # Look at the time evolution of the ROI brightness
+            current_brightness = np.sum(img_roi, axis=None)
+            del img_roi
+            gc.collect()
+            time_now = time.time() - time_start
+            time_elapsed.append(time_now)
+            roi_brightness.append(current_brightness)
+            if time_now > log_time:
+                deqtime = time_elapsed.popleft()
+                shotframes = len(time_elapsed)
+                deqtime = time_now - deqtime
+                roi_brightness.popleft()
                 gc.collect()
-                ax2.set_data(img_roi,0)
-                ax2.title('Region of Interest (x_1=%s, y_1=%s, w=%s, h=%s)'%(x_1,y_1,width,height))
-                if i == 1:
-                    cmap = cm.get_cmap('jet',4)
-                    bounds = np.linspace(0,1,21)
-                    ax2.colorbar(cmap=colors.ListedColormap(['b','g','y','r']),boundaries=bounds,norm=colors.BoundaryNorm(bounds,cmap.N))
-                ax2.tick_params(axis='x',which='both',top=False,labeltop=False,labelbottom=True)
-
-                # Look at the time evolution of the ROI brightness
-                current_brightness = np.sum(img_roi, axis=None)
-                del img_roi
-                gc.collect()
-                time_now = time.time() - time_start
-                time_elapsed.append(time_now)
-                roi_brightness.append(current_brightness)
-                if time_now > log_time:
-                    time_elapsed.popleft()
-                    roi_brightness.popleft()
-                    gc.collect()
-                ax3.clear()
-                ax3.plot(time_elapsed,roi_brightness,'b-')
-                ax3.xlim([time_now-log_time,time_now])
-                ax3.autoscale(True,axis='y')
-                ax3.xlabel('Elapsed Time [s]')
-                ax3.ylabel('Brightness [a.u.]')
-                ax3.title('Brightness within the ROI')
+                fps = float(shotframes)/deqtime
+                ax3.set_title(ax3_title + '(' + str(fps) + ' fps)')
+            else:
+                deqtime = 0.0
+                shotframes = len(time_elapsed)
+                deqtime = time_now - deqtime
+                fps = float(shotframes)/deqtime
+                ax3.set_title(ax3_title + '(' + str(fps) + ' fps)')
+            ax3.plot(time_elapsed,roi_brightness,'b-')
+            ax3.set_xlim([time_now-log_time,time_now])
+            ax3.autoscale(True,axis='y')
+            ax3.set_xlabel(ax3_xlabel)
+            ax3.set_ylabel(ax3_ylabel)
             
-#               # Create a STOP button to stop acquisition
-#               class Index(object):
-#                   ind = 0
-#                   def stop_acq(self, event):
-#                       self.ind += 1
-#                       raise SystemExit
-#               callback = Index()
-#               axstop = ax4.axes([0.7,0.05,0.1,0.075])
-#               bstop = Button(axstop, 'STOP')
-#               bstop.on_clicked(callback.stop_acq)
-
-            # Draw the Histo/Graph
-            histostream = animation.FuncAnimation(plt.gcf(), update, interval=100)
-            def close(event):
-                if event.key == 'q':
-                    plt.close(event.canvas.figure)
-                    raise SystemExit
-            cid = plt.gcf().canvas.mpl_connect("key_press_event", close)
-#            plt.tight_layout()
-            plt.show()
+            grabResult.Release()
 
         else:
             print("Error: ", grabResult.ErrorCode, grabResult.ErrorDescription)
-        grabResult.Release()
-    camera.Close()
-    time_elapsed.clear()
-    roi_brightness.clear()
+
+    # Draw the Histo/Graph
+    histostream = animation.FuncAnimation(plt.gcf(), update, interval=shoot_int, fargs = ('Obtained Image ', 'Region of Interest ', 'ROI Brightness ', 'Elapsed Time [s]', 'Brightness [a.u.]'))
+    def close(event):
+        if event.key == 'q':
+            plt.close(event.canvas.figure)
+            camera.StopGrabbing()
+            camera.Close()
+            time_elapsed.clear()
+            roi_brightness.clear()
+            raise SystemExit
+    cid = plt.gcf().canvas.mpl_connect("key_press_event", close)
+    plt.show()
+
 
 except genicam.GenericException as e:
     # Error handling.
