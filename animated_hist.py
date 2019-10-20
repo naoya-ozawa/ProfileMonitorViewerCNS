e6b7f2d81401c32ee5711bf2d22c9853997826c2
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2019-10-20 20:22:14 +0900

    [add] fps logger on ax4

--- animated_hist.py
+++ animated_hist.py
@@ -1,18 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import time
 
 def grab_frame():
     n = 1500
     number_of_frames = 1500
     return np.random.rand(n, number_of_frames)
 
-frame = []
+elapsetime = []
 brightness = []
+fps = []
+init_time = time.time()
 
 ax1 = plt.subplot(2,2,1)
 ax2 = plt.subplot(2,2,2)
 ax3 = plt.subplot(2,2,3)
+ax4 = plt.subplot(2,2,4)
 
 im1 = ax1.matshow(grab_frame())
 im2 = ax2.matshow(grab_frame())
@@ -21,11 +25,16 @@ def update(i):
     image = grab_frame()
     im1.set_data(image)
     im2.set_data(grab_frame())
-    frame.append(i)
+    current_time = time.time()
+    elapsetime.append(current_time-init_time)
+    current_fps = float(i)/(current_time-init_time)
+    fps.append(current_fps)
     b_now = np.sum(image, axis=None)
     brightness.append(b_now)
     ax3.clear()
-    ax3.plot(frame, brightness)
+    ax3.plot(elapsetime, brightness)
+    ax4.clear()
+    ax4.plot(elapsetime, fps)
 
 ani1 = animation.FuncAnimation(plt.gcf(), update, interval=50)
 
