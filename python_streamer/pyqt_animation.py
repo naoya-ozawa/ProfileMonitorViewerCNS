0d8108088bc5d57450263a68bf8b2ed8300273c5
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2019-10-28 18:32:14 +0900

    [add] docking segmentation of display window

--- python_streamer/pyqt_animation.py
+++ python_streamer/pyqt_animation.py
@@ -1,6 +1,9 @@
 from PyQt5 import QtWidgets, uic
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+from pyqtgraph.dockarea import *
+import numpy as np
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 
@@ -17,9 +20,24 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget.plot(hour, temperature)
     
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
+    app = QtGui.QApplication([])
+    win = QtGui.QMainWindow()
+    area = DockArea()
+    win.setCentralWidget(area)
+    win.resize(1000,500)
+    win.setWindowTitle('dockarea example')
+
+    # Create Docks
+    d1 = Dock("Dock1",size=(1,1))
+    d2 = Dock("Dock2 - Image",size=(500,200))
+    area.addDock(d1,'left')
+    area.addDock(d2,'right',d1)
+
+    w2 = pg.PlotWidget(title="Plot")
+    w2.plot(np.random.normal(size=100))
+    d2.addWidget(w2)
+    win.show()
+
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
